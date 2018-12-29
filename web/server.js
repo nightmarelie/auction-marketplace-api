729b1f258805e3603f3cc6c96b319dc4b0eb0045
Aleksey Kryvtsov <nightmarelie@gmail.com>
2018-12-29 16:16:50 +0200

    Rename main web dictionary

--- web/server.js
+++ web/server.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const app = express();
 const middlewares = require('./middlewares');
-const routers = require('./components');
+const routers = require('./domain');
 const path = require ('path');
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'components'));
+app.set('views', path.join(__dirname, 'domain'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
